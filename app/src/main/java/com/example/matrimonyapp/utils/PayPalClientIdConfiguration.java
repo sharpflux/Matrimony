42762f3a89243113458d16df1be5c9f77f0001de
Aparna <sharpflux@gmail.com>
2020-12-26 01:47:56 +0530

    Working on PayPal Integration

--- app/src/main/java/com/example/matrimonyapp/utils/PayPalClientIdConfiguration.java
+++ app/src/main/java/com/example/matrimonyapp/utils/PayPalClientIdConfiguration.java
@@ -1,5 +1,5 @@
 package com.example.matrimonyapp.utils;
 
 public class PayPalClientIdConfiguration {
-    public static final String CLIENT_ID = "AVLYBwu--8UXYJoKseceJY6g0ML7jIKeHKAPGNVShsOHjphCJO4BjPAqS9YE1EF7uHHMZ71nkJtEed1z";
+    public static final String CLIENT_ID = "AT99NvZ_MzSDdnCm8q4KsSMBvuVRs4o2XkHyYBZJ-dTYquoYbaTnwzg-BAaAt7X_bbj8k7mZ1tb9a4Wr";//"AVLYBwu--8UXYJoKseceJY6g0ML7jIKeHKAPGNVShsOHjphCJO4BjPAqS9YE1EF7uHHMZ71nkJtEed1z";
 }
